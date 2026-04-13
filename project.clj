8da0412183f6c0fefc15fcb7bffa11d5ab46670b
Dan Lentz <danlentz@gmail.com>
2026-04-13 19:10:10 -0400

    Bump clj-kondo to 2026.01.19 and fix project URL
    Update clj-kondo from 2024.08.01 to 2026.01.19 in both project.clj
    and deps.edn. Fix project :url from FIXME placeholder to
    github.com/danlentz/clj-xref.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject com.github.danlentz/clj-xref "0.1.0"
   :description "Cross-reference database for Clojure code"
-  :url "https://github.com/FIXME/clj-xref"
+  :url "https://github.com/danlentz/clj-xref"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.2"]
-                 [clj-kondo/clj-kondo "2024.08.01"]]
+                 [clj-kondo/clj-kondo "2026.01.19"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.1"]]}}
   :repl-options {:init-ns clj-xref.core})
