e3faab162f9b3195c3f00cdf3136fe4390ae7c16
Dan Lentz <danlentz@gmail.com>
2026-04-13 18:00:09 -0400

    Add adversarial and stress test suites
    adversarial_test: empty/partial databases, nil/unqualified symbols,
    single-segment and deep namespaces, special character symbols,
    mutual and self recursion, namespace cycles, duplicate refs/vars,
    zero arity, nonexistent/empty/corrupt EDN files.
    
    stress_test: index build timing at 500/2K/5K vars, query latency
    on 100K-ref database, EDN write/read throughput, file size bounds,
    repeated-query memory stability.

---
+++ test/clj_xref/adversarial_test.clj
@@ -0,0 +1,165 @@
+(ns clj-xref.adversarial-test
+  "Edge cases, boundary conditions, and malformed input."
+  (:require [clojure.test :refer [deftest is testing]]
+            [clj-xref.core :as xref]
+            [clj-xref.emit :as emit]
+            [clj-xref.test-utils :as tu :refer [make-var make-ref make-ns
+                                                 make-db make-indexed-db
+                                                 with-temp-edn]]))
+
+;; === Empty and minimal databases ===
+
+(deftest test-completely-empty-db
+  (let [db (make-indexed-db {:vars [] :refs [] :namespaces []})]
+    (is (= [] (xref/who-calls db 'a/b)))
+    (is (= [] (xref/calls-who db 'a/b)))
+    (is (= [] (xref/who-references db 'a/b)))
+    (is (= [] (xref/who-macroexpands db 'a/b)))
+    (is (= [] (xref/who-implements db 'a/b)))
+    (is (= [] (xref/who-dispatches db 'a/b)))
+    (is (= [] (xref/ns-vars db 'a)))
+    (is (= #{} (xref/ns-deps db 'a)))
+    (is (= #{} (xref/ns-dependents db 'a)))))
+
+(deftest test-refs-but-no-vars
+  (let [db (make-indexed-db
+             {:vars []
+              :refs [(make-ref :call 'a/f 'b/g)]
+              :namespaces []})]
+    (is (= 1 (count (xref/who-calls db 'b/g))))
+    (is (nil? (get (:vars-by-name db) 'a/f)))))
+
+(deftest test-vars-but-no-refs
+  (let [db (make-indexed-db
+             {:vars [(make-var 'a 'f)]
+              :refs []
+              :namespaces []})]
+    (is (= [] (xref/who-calls db 'a/f)))
+    (is (= [] (xref/calls-who db 'a/f)))
+    (is (= 1 (count (xref/ns-vars db 'a))))))
+
+(deftest test-no-namespaces
+  (let [db (make-indexed-db
+             {:vars [(make-var 'a 'f)]
+              :refs [(make-ref :call 'a/f 'b/g)]
+              :namespaces []})]
+    (is (= {} (:ns-by-name db)))
+    (is (= 1 (count (xref/who-calls db 'b/g))))))
+
+;; === Symbol edge cases ===
+
+(deftest test-unqualified-symbol
+  (is (= [] (xref/who-calls (make-indexed-db {:vars [] :refs [] :namespaces []}) 'foo))))
+
+(deftest test-nil-symbol
+  (let [db (make-indexed-db {:vars [] :refs [] :namespaces []})]
+    (is (= [] (xref/who-calls db nil)))
+    (is (= [] (xref/calls-who db nil)))
+    (is (= [] (xref/who-references db nil)))))
+
+(deftest test-single-segment-namespace
+  (let [db (make-indexed-db
+             {:vars [(make-var 'util 'foo)]
+              :refs [(make-ref :call 'util/foo 'util/foo)]
+              :namespaces [(make-ns 'util)]})]
+    (is (= 1 (count (xref/who-calls db 'util/foo))))))
+
+(deftest test-deep-namespace
+  (let [deep-ns 'a.b.c.d.e.f.g.h
+        db (make-indexed-db
+             {:vars [(make-var deep-ns 'foo)]
+              :refs [(make-ref :call (symbol (str deep-ns) "foo")
+                                     (symbol (str deep-ns) "foo"))]
+              :namespaces [(make-ns deep-ns)]})]
+    (is (= 1 (count (xref/ns-vars db deep-ns))))))
+
+(deftest test-special-character-symbols
+  (let [db (make-indexed-db
+             {:vars [(make-var 'my.ns '*earmuffs*)
+                     (make-var 'my.ns '->Widget)
+                     (make-var 'my.ns 'map->Widget)
+                     (make-var 'my.ns '<top-level>)
+                     (make-var 'my.ns 'foo?)
+                     (make-var 'my.ns 'foo!)]
+              :refs [(make-ref :call 'my.ns/foo? 'my.ns/*earmuffs*)]
+              :namespaces [(make-ns 'my.ns)]})]
+    (is (= 6 (count (xref/ns-vars db 'my.ns))))
+    (is (= 1 (count (xref/who-calls db 'my.ns/*earmuffs*))))))
+
+;; === Circular and self-referential ===
+
+(deftest test-mutual-recursion
+  (let [db (make-indexed-db
+             {:vars [(make-var 'a 'f) (make-var 'a 'g)]
+              :refs [(make-ref :call 'a/f 'a/g)
+                     (make-ref :call 'a/g 'a/f)]
+              :namespaces [(make-ns 'a)]})]
+    (is (= #{'a/g} (tu/froms (xref/who-calls db 'a/f))))
+    (is (= #{'a/f} (tu/froms (xref/who-calls db 'a/g))))
+    (is (= #{'a/g} (tu/tos (xref/calls-who db 'a/f))))
+    (is (= #{'a/f} (tu/tos (xref/calls-who db 'a/g))))))
+
+(deftest test-self-recursion
+  (let [db (make-indexed-db
+             {:vars [(make-var 'a 'f)]
+              :refs [(make-ref :call 'a/f 'a/f)]
+              :namespaces [(make-ns 'a)]})]
+    (is (= 1 (count (xref/who-calls db 'a/f))))
+    (is (= 1 (count (xref/calls-who db 'a/f))))))
+
+(deftest test-namespace-cycle
+  (let [db (make-indexed-db
+             {:vars [(make-var 'a 'f) (make-var 'b 'g)]
+              :refs [(make-ref :call 'a/f 'b/g)
+                     (make-ref :call 'b/g 'a/f)]
+              :namespaces [(make-ns 'a) (make-ns 'b)]})]
+    (is (contains? (xref/ns-deps db 'a) 'b))
+    (is (contains? (xref/ns-deps db 'b) 'a))
+    (is (contains? (xref/ns-dependents db 'a) 'b))
+    (is (contains? (xref/ns-dependents db 'b) 'a))))
+
+;; === Duplicate entries ===
+
+(deftest test-duplicate-refs
+  (let [db (make-indexed-db
+             {:vars [(make-var 'a 'f) (make-var 'b 'g)]
+              :refs [(make-ref :call 'a/f 'b/g)
+                     (make-ref :call 'a/f 'b/g)]
+              :namespaces []})]
+    (is (= 2 (count (xref/who-calls db 'b/g))))))
+
+(deftest test-duplicate-vars
+  (let [db (make-indexed-db
+             {:vars [(make-var 'a 'f :line 1)
+                     (make-var 'a 'f :line 99)]
+              :refs []
+              :namespaces []})]
+    ;; vars-by-name keeps the last one (map insert semantics)
+    (is (= 99 (:line (get (:vars-by-name db) 'a/f))))
+    ;; ns-vars returns both
+    (is (= 2 (count (xref/ns-vars db 'a))))))
+
+;; === Transform edge cases ===
+
+(deftest test-zero-arity
+  (let [db (make-indexed-db
+             {:vars []
+              :refs [(make-ref :call 'a/f 'b/g :arity 0)]
+              :namespaces []})]
+    (is (= 1 (count (xref/who-calls db 'b/g))))))
+
+;; === File system edge cases ===
+
+(deftest test-load-nonexistent-file
+  (is (thrown? java.io.FileNotFoundException
+        (xref/load-db "/no/such/file.edn"))))
+
+(deftest test-load-empty-file
+  (with-temp-edn [path]
+    (spit path "")
+    (is (thrown? Exception (emit/read-edn path)))))
+
+(deftest test-load-invalid-edn
+  (with-temp-edn [path]
+    (spit path "{:broken [}")
+    (is (thrown? Exception (emit/read-edn path)))))
---
+++ test/clj_xref/stress_test.clj
@@ -0,0 +1,96 @@
+(ns clj-xref.stress-test
+  "Performance and scale tests with large synthetic databases."
+  (:require [clojure.test :refer [deftest is testing]]
+            [clj-xref.core :as xref]
+            [clj-xref.emit :as emit]
+            [clj-xref.test-utils :as tu :refer [generate-large-db with-temp-edn timed]]))
+
+;; === Index build time ===
+
+(deftest test-index-time-small
+  (let [db (generate-large-db {:n-namespaces 10 :n-vars-per-ns 50 :n-refs-per-var 10})
+        {:keys [ms]} (timed #(xref/index db))]
+    (is (= 500 (count (:vars db))))
+    (is (= 5000 (count (:refs db))))
+    (is (< ms 2000) (str "Index build took " ms "ms, expected < 2000ms"))))
+
+(deftest test-index-time-medium
+  (let [db (generate-large-db {:n-namespaces 40 :n-vars-per-ns 50 :n-refs-per-var 10})
+        {:keys [ms]} (timed #(xref/index db))]
+    (is (= 2000 (count (:vars db))))
+    (is (= 20000 (count (:refs db))))
+    (is (< ms 5000) (str "Index build took " ms "ms, expected < 5000ms"))))
+
+(deftest test-index-time-large
+  (let [db (generate-large-db {:n-namespaces 100 :n-vars-per-ns 50 :n-refs-per-var 20})
+        {:keys [ms]} (timed #(xref/index db))]
+    (is (= 5000 (count (:vars db))))
+    (is (= 100000 (count (:refs db))))
+    (is (< ms 10000) (str "Index build took " ms "ms, expected < 10000ms"))))
+
+;; === Query time on large database ===
+
+(def large-db
+  (let [raw (generate-large-db {:n-namespaces 100 :n-vars-per-ns 50 :n-refs-per-var 20})]
+    (xref/index raw)))
+
+(deftest test-query-time-who-calls
+  ;; Pick a symbol that has many callers (the first var in ns0)
+  (let [{:keys [ms result]} (timed #(xref/who-calls large-db 'stress.ns0/fn0))]
+    (is (< ms 50) (str "who-calls took " ms "ms, expected < 50ms"))
+    (is (vector? result))))
+
+(deftest test-query-time-calls-who
+  (let [{:keys [ms result]} (timed #(xref/calls-who large-db 'stress.ns0/fn0))]
+    (is (< ms 50) (str "calls-who took " ms "ms, expected < 50ms"))
+    (is (vector? result))))
+
+(deftest test-query-time-who-references
+  (let [{:keys [ms result]} (timed #(xref/who-references large-db 'stress.ns0/fn0))]
+    (is (< ms 50) (str "who-references took " ms "ms, expected < 50ms"))
+    (is (vector? result))))
+
+(deftest test-query-time-ns-deps
+  (let [{:keys [ms result]} (timed #(xref/ns-deps large-db 'stress.ns0))]
+    (is (< ms 2000) (str "ns-deps took " ms "ms, expected < 2000ms"))
+    (is (set? result))))
+
+(deftest test-query-time-ns-dependents
+  (let [{:keys [ms result]} (timed #(xref/ns-dependents large-db 'stress.ns0))]
+    (is (< ms 2000) (str "ns-dependents took " ms "ms, expected < 2000ms"))
+    (is (set? result))))
+
+;; === EDN write/read time ===
+
+(deftest test-edn-write-time-medium
+  (let [db (generate-large-db {:n-namespaces 10 :n-vars-per-ns 50 :n-refs-per-var 10})]
+    (with-temp-edn [path]
+      (let [{:keys [ms]} (timed #(emit/write-edn db path))]
+        (is (< ms 5000) (str "Write 5K refs took " ms "ms, expected < 5000ms"))))))
+
+(deftest test-edn-read-time-medium
+  (let [db (generate-large-db {:n-namespaces 10 :n-vars-per-ns 50 :n-refs-per-var 10})]
+    (with-temp-edn [path]
+      (emit/write-edn db path)
+      (let [{:keys [ms]} (timed #(emit/read-edn path))]
+        (is (< ms 5000) (str "Read 5K refs took " ms "ms, expected < 5000ms"))))))
+
+(deftest test-edn-roundtrip-large
+  (let [db (generate-large-db {:n-namespaces 100 :n-vars-per-ns 50 :n-refs-per-var 20})]
+    (with-temp-edn [path]
+      (let [{write-ms :ms} (timed #(emit/write-edn db path))
+            {read-ms :ms}  (timed #(emit/read-edn path))
+            file-size (.length (java.io.File. path))]
+        (is (< write-ms 30000) (str "Write 100K refs took " write-ms "ms"))
+        (is (< read-ms 30000) (str "Read 100K refs took " read-ms "ms"))
+        ;; File size sanity check: should be under 30MB
+        (is (< file-size (* 30 1024 1024))
+            (str "File is " (/ file-size 1024 1024.0) "MB, expected < 30MB"))))))
+
+;; === Memory stability (smoke check) ===
+
+(deftest test-repeated-queries-no-crash
+  (dotimes [_ 100]
+    (xref/who-calls large-db 'stress.ns0/fn0)
+    (xref/calls-who large-db 'stress.ns0/fn0)
+    (xref/who-references large-db 'stress.ns0/fn0)))
