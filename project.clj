be73de2bd6b4455378cb9acee9053314b4a5d7c6
Dan Lentz <danlentz@gmail.com>
2026-04-14 06:27:59 -0400

    Add clj-format dependency and lein aliases
    Add com.github.danlentz/clj-format 0.1.1 as a dependency.
    Add lein aliases for xref and measure-improvement so both
    work during in-project development.

--- project.clj
+++ project.clj
@@ -4,8 +4,13 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.2"]
-                 [clj-kondo/clj-kondo "2026.01.19"]]
+                 [clj-kondo/clj-kondo "2026.01.19"]
+                 [com.github.danlentz/clj-format "0.1.1"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.1"]
                                   [clj-http "3.13.0"]]}}
+  :aliases {"xref" ["trampoline" "run" "-m" "clojure.main" "-e"
+                    "(require '[clj-xref.tool :as t]) (t/generate {:paths [\"src\"]})"]
+            "measure-improvement" ["trampoline" "run" "-m" "clojure.main" "-e"
+                                   "(require '[clj-xref.bench :as b]) (b/print-results (b/run-benchmark {:paths [\"src\"]}))"]}
   :signing  {:gpg-key "0CA466A1AB48F0C0264AF55307BAD70176C4B179"}
   :repl-options {:init-ns clj-xref.core})
